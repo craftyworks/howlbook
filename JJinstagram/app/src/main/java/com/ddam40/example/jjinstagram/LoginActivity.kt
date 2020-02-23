67b8b5873e17bfaa6ade0c7c9400a1e62fb16647
craftyworks <craftyworks@gmail.com>
2020-02-23 17:47:10 +0900

    Add files

--- JJinstagram/app/src/main/java/com/ddam40/example/jjinstagram/LoginActivity.kt
+++ JJinstagram/app/src/main/java/com/ddam40/example/jjinstagram/LoginActivity.kt
@@ -1,12 +1,49 @@
 package com.ddam40.example.jjinstagram
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.facebook.CallbackManager
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.twitter.sdk.android.core.identity.TwitterAuthClient
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
+    var auth: FirebaseAuth? = null
+    var googleSignInClient: GoogleSignInClient? = null
+    var callbackManager: CallbackManager? = null
+    val GOOGLE_LOGIN_CODE = 9001
+    var twitterAuthClient: TwitterAuthClient? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        callbackManager = CallbackManager.Factory.create()
+
+        google_sigin_in_button.setOnClickListener { googleLogin() }
+        facebook_login_button.setOnClickListener { facebookLogin() }
+        email_login_button.setOnClickListener { emailLogin() }
+        twitter_login_button.setOnClickListener { twitterLogin() }
+    }
+
+    fun moveMainPage(user: FirebaseUser?) {
+        if(user != null) {
+            Toast.makeText(this, getString(R.string.signin_complete), Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 }
