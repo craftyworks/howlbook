6ee16ee3220e7024696ae762f4a4d3fd8f437714
craftyworks <craftyworks@gmail.com>
2020-02-24 00:41:47 +0900

    Add files

--- JJinstagram/app/src/main/java/com/ddam40/example/jjinstagram/MainActivity.kt
+++ JJinstagram/app/src/main/java/com/ddam40/example/jjinstagram/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.ddam40.example.jjinstagram
 
+import android.Manifest
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        progress_bar.visibility = View.VISIBLE
+
+        bottom_navigation.setOnNavigationItemSelectedListener ( this )
+        bottom_navigation.selectedItemId = R.id.action_home
+
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+    }
+
+    fun setToolbarDefault() {
+        toolbar_title_image.visibility = View.VISIBLE
+        toolbar_btn_back.visibility = View.GONE
+        toolbar_username.visibility = View.GONE
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        setToolbarDefault()
+        when(item.itemId) {
+            R.id.action_home -> {
+            }
+        }
+        //FIXME
+        return false
     }
 }
